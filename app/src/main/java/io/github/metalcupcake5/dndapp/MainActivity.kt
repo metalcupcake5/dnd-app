b265ad41df832fa50043a97e9539681ae330613a
metalcupcake5 <imcoolcom5@gmail.com>
2021-04-23 10:55:38 -0700

    Initial commit

---
+++ app/src/main/java/io/github/metalcupcake5/dndapp/MainActivity.kt
@@ -0,0 +1,11 @@
+package io.github.metalcupcake5.dndapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
