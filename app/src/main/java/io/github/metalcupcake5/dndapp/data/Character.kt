3a7be0729a5a2a18eb88f6f06a08cdb08227292c
metalcupcake5 <imcoolcom5@gmail.com>
2021-04-26 11:56:51 -0700

    feat: add more data to character class

--- app/src/main/java/io/github/metalcupcake5/dndapp/data/Character.kt
+++ app/src/main/java/io/github/metalcupcake5/dndapp/data/Character.kt
@@ -4,6 +4,17 @@ data class Character(
     val name: String,
     val race: String,
     val className: String,
+    val background: String,
+    val alignment: String,
+    val xp: Int,
+    val strength: Int,
+    val dexterity: Int,
+    val constitution: Int,
+    val intelligence: Int,
+    val wisdom: Int,
+    val charisma: Int,
     val hp: Int,
-
+    val initiativeBonus: Int,
+    val armorClass: Int,
+    val speed: Int
 )
