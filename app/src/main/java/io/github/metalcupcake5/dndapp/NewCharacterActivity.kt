8ea6247c11502a1743d229b755aaeb58a89dc222
metalcupcake5 <imcoolcom5@gmail.com>
2021-04-30 10:50:37 -0700

    feat: add class dropdown menu

--- app/src/main/java/io/github/metalcupcake5/dndapp/NewCharacterActivity.kt
+++ app/src/main/java/io/github/metalcupcake5/dndapp/NewCharacterActivity.kt
@@ -5,8 +5,10 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.TextUtils
+import android.widget.ArrayAdapter
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Spinner
 
 class NewCharacterActivity : AppCompatActivity() {
     private lateinit var editCharacterView: EditText
@@ -14,16 +16,28 @@ class NewCharacterActivity : AppCompatActivity() {
     public override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_new_character)
-        editCharacterView = findViewById(R.id.edit_character)
+        editCharacterView = findViewById(R.id.textView_newCharacter_name)
+        val spinner: Spinner = findViewById(R.id.spinner_newCharacter_characterClass)
 
-        val button = findViewById<Button>(R.id.button_save)
+        ArrayAdapter.createFromResource(
+            this,
+            R.array.characterClasses,
+            android.R.layout.simple_spinner_item
+        ).also { adapter ->
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            spinner.adapter = adapter
+        }
+
+        val button = findViewById<Button>(R.id.button_newCharacter_save)
         button.setOnClickListener {
             val replyIntent = Intent()
             if (TextUtils.isEmpty(editCharacterView.text)) {
                 setResult(Activity.RESULT_CANCELED, replyIntent)
             } else {
-                val character = editCharacterView.text.toString()
-                replyIntent.putExtra(EXTRA_REPLY, character)
+                val characterName = editCharacterView.text.toString()
+                val characterClass = spinner.selectedItem.toString()
+                replyIntent.putExtra(EXTRA_NAME, characterName)
+                replyIntent.putExtra(EXTRA_CLASS, characterClass)
                 setResult(Activity.RESULT_OK, replyIntent)
             }
             finish()
@@ -31,7 +45,8 @@ class NewCharacterActivity : AppCompatActivity() {
     }
 
     companion object {
-        const val EXTRA_REPLY = "com.example.android.wordlistsql.REPLY"
+        const val EXTRA_NAME = "name"
+        const val EXTRA_CLASS = "class"
     }
 
 }
