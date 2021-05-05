3b8a60836fcc964fe3c33a86f17fcf992f208797
metalcupcake5 <imcoolcom5@gmail.com>
2021-05-05 14:47:02 -0700

    feat: move character addition to NewCharacterFragment.kt

--- app/src/main/java/io/github/metalcupcake5/dndapp/NewCharacterFragment.kt
+++ app/src/main/java/io/github/metalcupcake5/dndapp/NewCharacterFragment.kt
@@ -1,32 +1,30 @@
 package io.github.metalcupcake5.dndapp
 
+import android.content.Intent
 import android.os.Bundle
+import android.text.TextUtils
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.*
+import androidx.fragment.app.viewModels
+import androidx.navigation.findNavController
+import io.github.metalcupcake5.dndapp.data.Character
+import io.github.metalcupcake5.dndapp.data.CharacterApplication
+import io.github.metalcupcake5.dndapp.data.CharacterViewModel
+import io.github.metalcupcake5.dndapp.data.CharacterViewModelFactory
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [NewCharacterFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class NewCharacterFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+
+    private val characterViewModel: CharacterViewModel by viewModels {
+        CharacterViewModelFactory((requireActivity().application as CharacterApplication).repository)
+    }
+
+    private lateinit var editCharacterView: EditText
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
     }
 
     override fun onCreateView(
@@ -34,26 +32,41 @@ class NewCharacterFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_new_character, container, false)
+        val rootView = inflater.inflate(R.layout.fragment_new_character, container, false)
+
+        editCharacterView = rootView.findViewById(R.id.textView_newCharacter_name)
+        val spinner: Spinner = rootView.findViewById(R.id.spinner_newCharacter_characterClass)
+
+        ArrayAdapter.createFromResource(
+            requireActivity(),
+            R.array.characterClasses,
+            android.R.layout.simple_spinner_item
+        ).also { adapter ->
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            spinner.adapter = adapter
+        }
+
+        val button = rootView.findViewById<Button>(R.id.button_newCharacter_save)
+        button.setOnClickListener {
+            val replyIntent = Intent()
+            if (TextUtils.isEmpty(editCharacterView.text)) {
+                Toast.makeText(requireActivity(), "No name provided!", Toast.LENGTH_SHORT).show()
+            } else {
+                val characterName = editCharacterView.text.toString()
+                val characterClass = spinner.selectedItem.toString()
+                replyIntent.putExtra(NewCharacterActivity.EXTRA_NAME, characterName)
+                replyIntent.putExtra(NewCharacterActivity.EXTRA_CLASS, characterClass)
+                val character = Character(name = characterName,className = characterClass)
+                characterViewModel.insert(character)
+                view?.findNavController()?.navigate(R.id.action_newCharacterFragment_to_characterListFragment)
+            }
+        }
+        return rootView
     }
 
     companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment NewCharacterFragment.
-         */
-        // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            NewCharacterFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+            NewCharacterFragment()
     }
 }
