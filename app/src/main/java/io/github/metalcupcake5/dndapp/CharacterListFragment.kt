d5d7b2e4ba4e548916c4aefdf3c3267a222579c8
metalcupcake5 <imcoolcom5@gmail.com>
2021-05-05 11:08:27 -0700

    feat: add options menu to character list fragment

--- app/src/main/java/io/github/metalcupcake5/dndapp/CharacterListFragment.kt
+++ app/src/main/java/io/github/metalcupcake5/dndapp/CharacterListFragment.kt
@@ -1,11 +1,11 @@
 package io.github.metalcupcake5.dndapp
 
 import android.os.Bundle
+import android.util.Log
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
 import kotlinx.android.synthetic.main.fragment_character_list.*
 import kotlinx.android.synthetic.main.fragment_character_list.view.*
 
@@ -30,12 +30,24 @@ class CharacterListFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         val rootView = inflater.inflate(R.layout.fragment_character_list, container, false)
-        rootView.button_characterListFragment_newCharacter.setOnClickListener { view ->
+        /*rootView.button_characterListFragment_newCharacter.setOnClickListener { view ->
             view.findNavController().navigate(R.id.action_characterListFragment_to_newCharacterFragment)
-        }
+        }*/
+
+        setHasOptionsMenu(true)
 
         // Inflate the layout for this fragment
         return rootView
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.new_character_menu, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        view?.findNavController()?.navigate(R.id.action_characterListFragment_to_newCharacterFragment)
+        return super.onOptionsItemSelected(item)
     }
 
     companion object {
