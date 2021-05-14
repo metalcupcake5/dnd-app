7cf7cc6cf26a0997f7926e7ca1a785361a3e1511
metalcupcake5 <imcoolcom5@gmail.com>
2021-05-14 11:23:55 -0700

    feat: begin create character function

--- app/src/main/java/io/github/metalcupcake5/dndapp/data/CharacterViewModel.kt
+++ app/src/main/java/io/github/metalcupcake5/dndapp/data/CharacterViewModel.kt
@@ -28,6 +28,11 @@ class CharacterViewModel(private val repository: CharacterRepository) : ViewMode
     fun deleteCharacter(id: Int) = viewModelScope.launch {
         repository.deleteCharacter(id)
     }
+
+    fun createCharacter(name: String, className: String, race: String, stats: HashMap<String, Int>) = viewModelScope.launch {
+        val character = Character(name = name,className = className,race = race)
+        repository.insert(character)
+    }
 }
 class CharacterViewModelFactory(private val repository: CharacterRepository) : ViewModelProvider.Factory {
     override fun <T : ViewModel> create(modelClass: Class<T>): T {
