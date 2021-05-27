97334601134523d3bd0479f1717750d9c84d59a4
metalcupcake5 <imcoolcom5@gmail.com>
2021-05-26 23:58:46 -0700

    fix: change NewCharacterFragment.kt to include race data

--- app/src/main/java/io/github/metalcupcake5/dndapp/data/Race.kt
+++ app/src/main/java/io/github/metalcupcake5/dndapp/data/Race.kt
@@ -1,9 +1,11 @@
 package io.github.metalcupcake5.dndapp.data
 
 data class Race(
-    val ability_bonuses: List<AbilityBonuses>,
     val index: String,
     val name: String,
+    val speed: Int,
+    val ability_bonuses: List<AbilityBonuses>,
     val size: String,
-    val speed: Int
+    val ability_bonus_options: AbilityBonusOptions? = null
+
 )
